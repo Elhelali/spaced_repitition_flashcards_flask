fd1965012882285209a65c4c47087b37c01a584c
talkifier1 <ali.elhelali12@gmail.com>
2023-05-13 10:03:29 -0400

    add build query tests

---
+++ flashcards/test.py
@@ -0,0 +1,44 @@
+import unittest
+from utils import *
+
+class TestBuildQuery(unittest.TestCase):
+
+    def test_build_update_query(self):
+        # Test when successful is True
+        query = build_update_query(1, True)
+        self.assertIn("$set", query)
+        self.assertIn("$inc", query)
+        self.assertIn("words.$[elem].last_answer", query["$set"])
+        self.assertIn("words.$[elem].bin", query["$inc"])
+        self.assertNotIn("words.$[elem].wrong_count", query["$inc"])
+        self.assertEqual(query["$inc"]["words.$[elem].bin"], 1)
+
+        # Test when successful is False
+        query = build_update_query(1, False)
+        self.assertIn("$set", query)
+        self.assertIn("$inc", query)
+        self.assertIn("words.$[elem].last_answer", query["$set"])
+        self.assertIn("words.$[elem].bin", query["$inc"])
+        self.assertIn("words.$[elem].wrong_count", query["$inc"])
+        self.assertEqual(query["$inc"]["words.$[elem].bin"], 1)
+        self.assertEqual(query["$inc"]["words.$[elem].wrong_count"], 1)
+
+    def test_determine_bin_increment(self):
+        # Test when successful is True
+        query = determine_bin_increment(1, True)
+        self.assertEqual(query["$inc"]["words.$[elem].bin"], 1)
+        
+        # Test when successful is False and bin is 0
+        query = determine_bin_increment(0, False)
+        self.assertEqual(query["$inc"]["words.$[elem].bin"], 1)
+
+        # Test when successful is False and bin is 1
+        query = determine_bin_increment(1, False)
+        self.assertEqual(query["$inc"]["words.$[elem].bin"], 0)
+
+        # Test when successful is False and bin is greater than 1
+        query = determine_bin_increment(2, False)
+        self.assertEqual(query["$inc"]["words.$[elem].bin"], -1)
+
+if __name__ == '__main__':
+    unittest.main()
